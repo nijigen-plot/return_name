7446f7c6383f8f93fea354ff40085179115b8d78
nijigen-plot <quark_hardcore@yahoo.co.jp>
2021-07-24 14:36:00 +0900

    setup.pyのentry_pointsを追加

--- setup.py
+++ setup.py
@@ -4,5 +4,10 @@ setuptools.setup(
     name='returnname',
     version='1.0',
     description='test for pip install git+',
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
+    entry_points={
+        'console_scripts':[
+            'return = returnname.return.call'
+        ]
+    }
 )
