8c2a59648b0dfcee34c6ecfcb64d71a2c1036014
nijigen-plot <quark_hardcore@yahoo.co.jp>
2021-07-24 14:08:03 +0900

    pip install に必要なファイルを追加

---
+++ returnname/return.py
@@ -0,0 +1,2 @@
+def call(x):
+    return x
---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name='nijigen-plot',
+    version='1.0',
+    description='test for pip install git+',
+    packages=setuptools.find_packages()
+)
